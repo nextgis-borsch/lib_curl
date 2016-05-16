f4f385d0547b191e9c2515d25b844bed8505ccb9
Dmitry Baryshnikov <bishop.dev@gmail.com>
2016-05-16 12:59:22 +0300

    Fix other tests

--- CMake/OtherTests.cmake
+++ CMake/OtherTests.cmake
@@ -191,7 +191,7 @@ check_c_source_compiles("
   int main(void) {
     return poll((void *)0, 0, 10 /*ms*/);
   }" HAVE_POLL_FINE)
-endif()
+else()
 check_c_source_runs("
   #ifdef HAVE_SYS_POLL_H
   #  include <sys/poll.h>
