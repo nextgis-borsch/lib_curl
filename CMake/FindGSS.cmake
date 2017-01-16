d0ba0c0104609f3fbc96c78a309ff3da125989cb
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-01-16 12:43:14 +0300

    Fix zlib reporting in version

--- CMake/FindGSS.cmake
+++ CMake/FindGSS.cmake
@@ -68,7 +68,7 @@ message(STATUS "CFLAGS: ${_GSS_CFLAGS}")
             # should also work in an odd case when multiple directories are given
             string(STRIP "${_GSS_CFLAGS}" _GSS_CFLAGS)
             string(REGEX REPLACE " +-I" ";" _GSS_CFLAGS "${_GSS_CFLAGS}")
-            string(REGEX REPLACE " +-([^I][^ \\t;]*)" ";-\\1"_GSS_CFLAGS "${_GSS_CFLAGS}")
+            string(REGEX REPLACE " +-([^I][^ \\t;]*)" ";-\\1" _GSS_CFLAGS "${_GSS_CFLAGS}")
 
             foreach(_flag ${_GSS_CFLAGS})
                 if(_flag MATCHES "^-I.*")
@@ -90,7 +90,7 @@ message(STATUS "LDFLAGS: ${_GSS_LIB_FLAGS}")
             # this script gives us libraries and link directories. Blah. We have to deal with it.
             string(STRIP "${_GSS_LIB_FLAGS}" _GSS_LIB_FLAGS)
             string(REGEX REPLACE " +-(L|l)" ";-\\1" _GSS_LIB_FLAGS "${_GSS_LIB_FLAGS}")
-            string(REGEX REPLACE " +-([^Ll][^ \\t;]*)" ";-\\1"_GSS_LIB_FLAGS "${_GSS_LIB_FLAGS}")
+            string(REGEX REPLACE " +-([^Ll][^ \\t;]*)" ";-\\1" _GSS_LIB_FLAGS "${_GSS_LIB_FLAGS}")
 
             foreach(_flag ${_GSS_LIB_FLAGS})
                 if(_flag MATCHES "^-l.*")
