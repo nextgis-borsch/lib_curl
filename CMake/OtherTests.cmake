40aede642dd3a5a8ad402a07c6f4d81f051ba861
Dmitry Baryshnikov <bishop.dev@gmail.com>
2018-09-10 11:22:02 +0300

    Update to 7.61.1

--- CMake/OtherTests.cmake
+++ CMake/OtherTests.cmake
@@ -185,15 +185,6 @@ if(NOT APPLE)
   if(HAVE_SYS_POLL_H)
     set(CMAKE_REQUIRED_FLAGS "-DHAVE_SYS_POLL_H")
   endif()
-  if(ANDROID) # Crosscompile not support testrun
-      check_c_source_compiles("
-        #ifdef HAVE_SYS_POLL_H
-        #  include <sys/poll.h>
-        #endif
-        int main(void) {
-          return poll((void *)0, 0, 10 /*ms*/);
-        }" HAVE_POLL_FINE)
-  else()
   check_c_source_runs("
     #ifdef HAVE_SYS_POLL_H
     #  include <sys/poll.h>
@@ -201,7 +192,6 @@ if(NOT APPLE)
     int main(void) {
       return poll((void *)0, 0, 10 /*ms*/);
     }" HAVE_POLL_FINE)
-  endif()
 endif()
 
 set(HAVE_SIG_ATOMIC_T 1)
